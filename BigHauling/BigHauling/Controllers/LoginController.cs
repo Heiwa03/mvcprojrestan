92ed17dd2969575125efa87146fc95d0b886df54
Heiwa03 <vlad.rusu@isa.utm.md>
2025-06-27 19:11:21 +0300

    Create necessary constructs for Session creation within previously stated BL rules

---
+++ BigHauling/BigHauling/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigHauling.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly ISession _session;
+        public LoginController()
+        {
+            var bl = new BusinessLogic();
+            _session = bl.GetSessionBL();
+        }
+
+        // GET: Login
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BigHauling/BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,17 @@
+﻿using BigHauling.BusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigHauling.BusinessLogic
+{
+    public class BusinessLogic
+    {
+        public ISession GetSessionBL()
+        {
+            return new SessionBL();
+        }
+    }
+}
