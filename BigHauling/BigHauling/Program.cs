2721e4e5540ce59af9afcb62d1a99bfa5aadd9b3
Heiwa03 <vlad.rusu@isa.utm.md>
2025-06-27 20:51:05 +0300

    Reconfigure project for new architecture

--- BigHauling/BigHauling/Program.cs
+++ BigHauling/BigHauling/Program.cs
@@ -1,35 +1,43 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 using BigHauling.BusinessLogic.Data;
-using BigHauling.BusinessLogic.Interfaces;
-using BigHauling.BusinessLogic.Core;
 using BigHauling.Domain.Entities;
+using BigHauling.Helpers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+// Even though we don't inject UserManager/SignInManager into controllers,
+// the framework itself needs them registered in the DI container to function.
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
+    // These paths must be correctly set for authentication to work
     options.LoginPath = "/Account/Login";
     options.AccessDeniedPath = "/Account/AccessDenied";
 });
 
-builder.Services.AddScoped<IAdminAPI, AdminAPI>();
-
 var app = builder.Build();
 
+// Populate the service locator. This is the bridge that allows our
+// direct-instantiation BusinessLogic class to access framework services.
+ServiceActivator.ServiceProvider = app.Services;
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -41,10 +49,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// These are essential for Identity to work.
 app.UseAuthentication();
 app.UseAuthorization();
 
-// Seed the database
+// Seed the database with roles and the admin user.
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
